46e84e8586134320aa207e84c9eb3c90954c69d0
Ethan N <19988843+lucky15987@users.noreply.github.com>
2020-02-14 15:50:27 -0500

    Removed unnecessary vars

--- a1/js/dateTime.js
+++ a1/js/dateTime.js
@@ -1,14 +1,10 @@
 let d = new Date();
 
-/*
-var day = d.getDay();
-var month = d.getMonth();
-var numDay = d.getDate();
- */
+
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["January", "February", "March", "April",
     "May", "June", "July", "August",
     "September", "October", "November", "December"];
-var time = d.getTime();
+
 document.getElementById("datetime").innerHTML = days[d.getDay()] + ", " + months[d.getMonth()] + " " + d.getDate() + ", "
                         + d.toLocaleTimeString();
