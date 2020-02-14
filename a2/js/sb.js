f7fe6d57a124c1f653b9d72182a3cded9f5f0d8f
Ethan N <19988843+lucky15987@users.noreply.github.com>
2020-02-14 17:52:17 -0500

    Reference the js file. Added the "onchange" event listener for checkbox

---
+++ a2/js/sb.js
@@ -0,0 +1,13 @@
+checkbox.addEventListener('change', function ()
+{
+    if(this.checked)
+    {
+        document.querySelector(".nfo2 input[name=name").value = document.querySelector(".nfo input[name=name]").value;
+        document.querySelector(".nfo2 input[name=zip").value = document.querySelector(".nfo input[name=zip]").value;
+    }
+    else
+    {
+        document.querySelector(".nfo2 input[name=name").value = "";
+        document.querySelector(".nfo2 input[name=zip").value = "";
+    }
+});
